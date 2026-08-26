4e158ab5f2ddd53cb2cf51795f5d044433a7d686
VolTorian <33360939+VolTorian@users.noreply.github.com>
2026-08-26 00:39:19 -0700

    add game publisher validator
    makes sure that the selected publisher actually exists.
    shouldn't be a problem for normal users anyway since only
    the existing publishers should show up in the dropdown,
    but for malicious users...?

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,6 +4,7 @@ const db = require("../db/queries");
 async function addGamesGet(req, res) {
     const genresList = await db.getAllGenres();
     const publishersList = await db.getAllPublishers();
+    // console.log(publishersList)
 
     res.render("games", { genres: genresList, publishers: publishersList });
 }
@@ -13,8 +14,22 @@ const validateTitle = [
         .isLength({ min: 1, max: 255 }).withMessage("Game title is required")
 ]
 
+const validateChosenPublisher = [
+    body("chosenPublisher").custom(async (publisher) => {
+        const publishersList = await db.getAllPublishers();
+
+        if (!publishersList.map(item => item.publisher_name).includes(publisher)) {
+            throw new Error("Publisher not found. Please add it first");
+        }
+        else {
+            console.log("valid existing publisher selected")
+        }
+    })
+]
+
 addGamesPost = [
     validateTitle,
+    validateChosenPublisher,
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -27,6 +42,9 @@ addGamesPost = [
                 publishers: publishersList
             });
         }
+        console.log("SUBMITTED")
+        const { newGame, test2} = matchedData(req);
+        console.log(matchedData(req))
 
         res.redirect("/");
     }
